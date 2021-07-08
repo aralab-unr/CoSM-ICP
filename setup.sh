df433a12e97a561a0984329fbfa5d767b20fd495
Advanced Robotics and Automation Lab <43550092+aralab-unr@users.noreply.github.com>
2021-07-08 11:29:24 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,13 @@ echo "current directory: "$ORIG_DIR
 mkdir $ORIG_DIR/external_libraries/pcl_1_9/build/
 mkdir $ORIG_DIR/external_libraries/pcl_1_9/install_dir/
 PCL_DIR=$ORIG_DIR/external_libraries/pcl_1_9/
-cd $PCL_DIR/build
+cd $PCL_DIR/build/
 cmake -DCMAKE_INSTALL_PREFIX=$PCL_DIR/install_dir/ -DBUILD_CUDA=ON -DBUILD_GPU=ON ..
-make -j4
-make install
+make -j4 install
 
+cd $ORIG_DIR/src/
+cmake ..
+make -j4
+./CoSM_ICP_demo_Viewer bun01.pcd 1 0.005
 
 
